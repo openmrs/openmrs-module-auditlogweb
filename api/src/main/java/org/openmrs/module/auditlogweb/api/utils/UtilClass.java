6eca8ff292c4940419f53b3b0f64b4110a4e00fc
nsalifu <njiddasalifu@gmail.com>
2025-06-04 09:42:22 +0100

    Replaced previous annotation scanning logic with the Reflections library for simplicity and reliability

--- api/src/main/java/org/openmrs/module/auditlogweb/api/utils/UtilClass.java
+++ api/src/main/java/org/openmrs/module/auditlogweb/api/utils/UtilClass.java
@@ -9,63 +9,36 @@
 package org.openmrs.module.auditlogweb.api.utils;
 
 import org.hibernate.envers.Audited;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
-import org.springframework.core.type.classreading.MetadataReader;
-import org.springframework.core.type.classreading.MetadataReaderFactory;
-import org.springframework.util.ClassUtils;
-import org.springframework.util.SystemPropertyUtils;
-import java.io.IOException;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class UtilClass {
     private static List<String> classesWithAuditAnnotation;
 
-    public static List<String> findClassesWithAuditedAnnotation() throws IOException {
+    public static List<String> findClassesWithAnnotation() {
         if (classesWithAuditAnnotation != null) {
             return classesWithAuditAnnotation;
         }
 
-        ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
-        MetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory(resourcePatternResolver);
+        Reflections reflections = new Reflections(
+                new ConfigurationBuilder()
+                        .setUrls(ClasspathHelper.forPackage("org.openmrs"))
+                        .setScanners(Scanners.TypesAnnotated)
+        );
 
-        //Search only for Service Classes in DWR package.
-        String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + resolveBasePackage() + "/**/*.class";
-
-        List<String> candidateClasses = new ArrayList<>();
-        Resource[] resources = resourcePatternResolver.getResources(packageSearchPath);
-        for (Resource resource : resources) {
-            if (resource.isReadable()) {
-                MetadataReader metadataReader = metadataReaderFactory.getMetadataReader(resource);
-                if (doesClassContainsAuditedAnnotation(metadataReader)) {
-                    candidateClasses.add(metadataReader.getClassMetadata().getClassName());
-                }
-            }
-        }
-
-        classesWithAuditAnnotation = candidateClasses.stream()
-                .sorted(Comparator.naturalOrder())
+        Set<Class<?>> auditedClasses = reflections.getTypesAnnotatedWith(Audited.class);
+        classesWithAuditAnnotation = auditedClasses.stream()
+                .map(Class::getName)
+                .sorted()
                 .collect(Collectors.toList());
         return classesWithAuditAnnotation;
     }
-
-    private static String resolveBasePackage() {
-        return ClassUtils.convertClassNameToResourcePath(SystemPropertyUtils.resolvePlaceholders("org.openmrs"));
-    }
-
-    public static boolean doesClassContainsAuditedAnnotation(MetadataReader metadataReader) {
-        try {
-            Class<?> dwrClass = Class.forName(metadataReader.getClassMetadata().getClassName());
-
-            if (dwrClass.isAnnotationPresent(Audited.class)) {
-                return true;
-            }
-        } catch (Throwable ignored) {}
-        return false;
+    public static boolean doesClassContainsAuditedAnnotation(Class<?> clazz) {
+        return clazz.isAnnotationPresent(Audited.class);
     }
 }
