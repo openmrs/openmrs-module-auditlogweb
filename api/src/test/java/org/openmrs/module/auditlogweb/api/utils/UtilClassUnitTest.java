2d45da56fef9caf22d8217640c5666d09e5079c9
nsalifu <njiddasalifu@gmail.com>
2025-06-04 11:09:27 +0100

    added license header to the Test class and use assertFalse for not NotAuditedClass check

--- api/src/test/java/org/openmrs/module/auditlogweb/api/utils/UtilClassUnitTest.java
+++ api/src/test/java/org/openmrs/module/auditlogweb/api/utils/UtilClassUnitTest.java
@@ -1,9 +1,19 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.module.auditlogweb.api.utils;
 
 import org.hibernate.envers.Audited;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UtilClassUnitTest {
@@ -18,7 +28,7 @@ public class UtilClassUnitTest {
     @Test
     public void doesClassContainsAuditedAnnotation() {
         assertTrue(UtilClass.doesClassContainsAuditedAnnotation(TestAuditedClass.class));
-        assertTrue(!UtilClass.doesClassContainsAuditedAnnotation(NotAuditedClass.class));
+        assertFalse(UtilClass.doesClassContainsAuditedAnnotation(NotAuditedClass.class));
     }
 
     // Dummy Audited class for testing only
