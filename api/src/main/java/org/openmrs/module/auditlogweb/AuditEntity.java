6a12e1a2a6e91d871c541fc323872bfda851a355
njidda salifu <njiddasalifu@gmail.com>
2025-06-11 11:41:35 +0100

    (AUDIT-19): Added lombok dependency and refactore classes to use lombok annotations (#14)
    * Added lombok dependency and refactore classes to use lombok annotations
    
    * Add lombok  version as a property
    
    * Using @Data annotation from lombok, added the  missing version in pom.xml

--- api/src/main/java/org/openmrs/module/auditlogweb/AuditEntity.java
+++ api/src/main/java/org/openmrs/module/auditlogweb/AuditEntity.java
@@ -8,9 +8,15 @@
  */
 package org.openmrs.module.auditlogweb;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.envers.RevisionType;
 import org.openmrs.api.db.hibernate.envers.OpenmrsRevisionEntity;
 
+@Data
+@AllArgsConstructor
 public class AuditEntity<T> {
 
     private T entity;
@@ -21,46 +27,4 @@ public class AuditEntity<T> {
 
     private String changedBy;
 
-    public AuditEntity(T entity) {
-        this.entity = entity;
-    }
-
-    public AuditEntity(T entity, OpenmrsRevisionEntity revisionEntity, RevisionType revisionType, String changedBy) {
-        this.entity = entity;
-        this.revisionEntity = revisionEntity;
-        this.revisionType = revisionType;
-        this.changedBy = changedBy;
-    }
-
-    public T getEntity() {
-        return entity;
-    }
-
-    public void setEntity(T entity) {
-        this.entity = entity;
-    }
-
-    public OpenmrsRevisionEntity getRevisionEntity() {
-        return revisionEntity;
-    }
-
-    public void setRevisionEntity(OpenmrsRevisionEntity revisionEntity) {
-        this.revisionEntity = revisionEntity;
-    }
-
-    public RevisionType getRevisionType() {
-        return revisionType;
-    }
-
-    public void setRevisionType(RevisionType revisionType) {
-        this.revisionType = revisionType;
-    }
-
-    public String getChangedBy() {
-        return changedBy;
-    }
-
-    public void setChangedBy(String changedBy) {
-        this.changedBy = changedBy;
-    }
 }
