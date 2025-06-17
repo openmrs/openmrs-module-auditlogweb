9f09990f8528f12c00164712f7301dbc6e7a6653
nsalifu <njiddasalifu@gmail.com>
2025-06-17 12:26:17 +0100

    Add AuditlogwebConstants class to centralize module path constant

---
+++ omod/src/main/java/org/openmrs/module/auditlogweb/AuditlogwebConstants.java
@@ -0,0 +1,6 @@
+package org.openmrs.module.auditlogweb;
+
+public final class AuditlogwebConstants {
+    public static final String MODULE_PATH = "/module/auditlogweb";
+    private AuditlogwebConstants() {}
+}
