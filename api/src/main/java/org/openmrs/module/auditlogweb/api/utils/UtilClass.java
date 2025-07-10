144157a9560003852f3924a7952cc4df3cde587e
nsalifu <njiddasalifu@gmail.com>
2025-07-10 11:51:18 +0100

    remove wildcards importsin UtilClass

--- api/src/main/java/org/openmrs/module/auditlogweb/api/utils/UtilClass.java
+++ api/src/main/java/org/openmrs/module/auditlogweb/api/utils/UtilClass.java
@@ -22,7 +22,11 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.List;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Date;
 import java.util.stream.Collectors;
 
 /**
