4ceb3fd15148e6d465baf9741acc7de75b69865a
sudhanshu_raj <rajsudhanshu9431@gmail.com>
2026-06-05 17:32:49 +0530

    AUDIT-47 : Added missing imports

--- api/src/test/java/org/openmrs/module/auditlogweb/api/dao/AuditDaoTest.java
+++ api/src/test/java/org/openmrs/module/auditlogweb/api/dao/AuditDaoTest.java
@@ -52,6 +52,8 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
 import static org.mockito.ArgumentMatchers.anyString;
 
